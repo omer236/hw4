515ded244920922530c1fd5fbc064631a558b254
omer236 <81875949+omer236@users.noreply.github.com>
2022-01-20 19:15:27 +0000

    Add files via upload

--- malloc_2.cpp
+++ malloc_2.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
 #define MAX_SIZE 100000000
 typedef struct MallocMetadata_t {
     size_t size;
@@ -23,28 +26,17 @@ void* smalloc(size_t size){
         }
         ptr=ptr->next;
     }
-    MallocMetadata *sbrk_res=(MallocMetadata*)sbrk(size+sizeof(MallocMetadata));
+    void *pointer_sbrk=sbrk(size+sizeof(MallocMetadata));
+    MallocMetadata *sbrk_res=(MallocMetadata*)pointer_sbrk;
     sbrk_res->size=size;
     sbrk_res->is_free=false;
-    sbrk_res->block_addr=sbrk_res;
-    if(head== nullptr){
-        head=sbrk_res;
-    }
-    if(sbrk_res==(void*)-1){
-        return NULL;
-    }
-    return sbrk_res;
-}
-void* scalloc(size_t num, size_t size){
-
-}
-void sfree(void* p){
+    sbrk_res->block_addr=pointer_sbrk;
     if(head== nullptr){
         head=sbrk_res;
         return sbrk_res;
     }
     ptr=head;
-    while(ptr->block_addr<=sbrk_res){
+    while(ptr->block_addr<=ptr->block_addr){
         ptr=ptr->next;
     }
     if(ptr!= nullptr){
@@ -53,17 +45,78 @@ void sfree(void* p){
         ptr->prev=sbrk_res;
         sbrk_res->next=ptr;
     }
+    if(sbrk_res==(void*)-1){
+        return NULL;
+    }
+    return pointer_sbrk;
+}
+void* scalloc(size_t num, size_t size){
+    MallocMetadata *ptr;
+    ptr= (MallocMetadata*)smalloc(num*size);
+    if(ptr==NULL){
+        return NULL;
+    }
+    memset(ptr, 0, num*size);
+    return ptr;
+}
+void sfree(void* p){
+    MallocMetadata *current=(MallocMetadata*)p;
+    current->is_free= true;
+    if(head== nullptr){
+        head=current;
+        return;
+    }
+    MallocMetadata *ptr=head;
+    while(ptr->block_addr<=current->block_addr){
+        ptr=ptr->next;
+    }
+    if(ptr!= nullptr){
+        current->prev=ptr->prev;
+        ptr->prev->next=current;
+        ptr->prev=current;
+        current->next=ptr;
+    }
 }
 void* srealloc(void* oldp, size_t size){
-
+    MallocMetadata *ptr;
+    MallocMetadata *old=(MallocMetadata*)oldp;
+    if(old->size>size){
+        return oldp;
+    }
+    if(oldp==NULL){
+        return smalloc(size);
+    }
+    ptr= (MallocMetadata*)smalloc(size);
+    if(ptr==NULL){
+        return NULL;
+    }
+    memcpy(ptr, oldp, size);
+    sfree(oldp);
+    return ptr;
 }
 
 
 size_t _num_free_blocks(){
-
+    MallocMetadata *ptr=head;
+    int count=0;
+    while(ptr!=NULL){
+        if(ptr->is_free) {
+            count++;
+        }
+        ptr=ptr->next;
+    }
+    return count;
 }
 size_t _num_free_bytes(){
-
+    MallocMetadata *ptr=head;
+    int sum_bytes=0;
+    while(ptr!=NULL){
+        if(ptr->is_free) {
+            sum_bytes += ptr->size;
+        }
+        ptr=ptr->next;
+    }
+    return sum_bytes;
 }
 size_t _num_allocated_blocks(){
 
